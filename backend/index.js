b1b9d3b53ebca2f3763522046d1a6eab37fd4ac6
antonio <antoniopenev23@gmail.com>
2024-09-26 16:42:54 +0300

    Fixed image upload link

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@ app.use('/images', express.static('upload/images'));
 app.post('/upload', upload.single('product'), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://e-comm-3ab.pages.dev/images/${req.file.filename}`
+    image_url: `https://e-commerce-8wzd.onrender.com/images/${req.file.filename}`
   });
 });
 
